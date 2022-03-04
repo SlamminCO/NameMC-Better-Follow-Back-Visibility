57493aa82b8e025ff88994a10c6a08e6fb9c6387
SlamminCO <100872889+SlamminCO@users.noreply.github.com>
2022-03-04 11:13:08 -0500

    Error fixes
    Updated the manifest to version 3 and incremented the plugin version. Added check in content.js to prevent errors.

--- content.js
+++ content.js
@@ -24,35 +24,38 @@ sp1.innerHTML = "<a>Follows You</a>"
 let sp2_1 = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.border-bottom-0.d-none.d-sm-table-cell")
 let sp2 = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.d-none.d-sm-table-cell.text-nowrap")
 
-let parentDiv_1 = sp2_1.parentNode
-let parentDiv = sp2.parentNode
+if (sp2_1 && sp2) {
 
-parentDiv_1.insertBefore(sp1_1, sp2_1)
-parentDiv.insertBefore(sp1, sp2)
+    let parentDiv_1 = sp2_1.parentNode
+    let parentDiv = sp2.parentNode
+
+    parentDiv_1.insertBefore(sp1_1, sp2_1)
+    parentDiv.insertBefore(sp1, sp2)
 
 
 
-let list = document.querySelectorAll("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > tbody > tr")
+    let list = document.querySelectorAll("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > tbody > tr")
 
-for (let user of list) {
-    if (user.innerHTML.search("bold") == -1) {
-        let sp1 = document.createElement("td")
-        sp1.innerHTML = '<td title="Not Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/274c.svg" alt="❌"> <b>No</b></td>'
-        
-        let sp2 = user.getElementsByTagName("td")[3]
-        let parentDiv = sp2.parentNode
-        
-        parentDiv.insertBefore(sp1, sp2)
+    for (let user of list) {
+        if (user.innerHTML.search("bold") == -1) {
+            let sp1 = document.createElement("td")
+            sp1.innerHTML = '<td title="Not Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/274c.svg" alt="❌"> <b>No</b></td>'
+            
+            let sp2 = user.getElementsByTagName("td")[3]
+            let parentDiv = sp2.parentNode
+            
+            parentDiv.insertBefore(sp1, sp2)
 
-		let button = user.getElementsByClassName("btn btn-block btn-sm btn-success")
-		button[0].setAttribute("class", "btn btn-block btn-sm btn-success btn-not-follower")
-	} else {
-        let sp1 = document.createElement("td")
-        sp1.innerHTML = '<td title="Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/2705.svg" alt="✅"> <b>Yes</b></td>'
-        
-        let sp2 = user.getElementsByTagName("td")[3]
-        let parentDiv = sp2.parentNode
-        
-        parentDiv.insertBefore(sp1, sp2)
+            let button = user.getElementsByClassName("btn btn-block btn-sm btn-success")
+            button[0].setAttribute("class", "btn btn-block btn-sm btn-success btn-not-follower")
+        } else {
+            let sp1 = document.createElement("td")
+            sp1.innerHTML = '<td title="Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/2705.svg" alt="✅"> <b>Yes</b></td>'
+            
+            let sp2 = user.getElementsByTagName("td")[3]
+            let parentDiv = sp2.parentNode
+            
+            parentDiv.insertBefore(sp1, sp2)
+        }
     }
 }
