21f8221e479cb9ec2bca025bdd96fece522a0469
SlamminCO <100872889+SlamminCO@users.noreply.github.com>
2022-03-11 00:04:59 -0500

    Feature Update 1.3
    Added an Unfollow Non-Followers button for individual pages, Follow back visibility now shows on following pages regardless of ownership, rewrote content.js for better readability

--- content.js
+++ content.js
@@ -1,53 +1,106 @@
-chrome.storage.sync.get("autoUnfollow", function (data) {
-    let autoUnfollow = data["autoUnfollow"]
+let unfollowAllButton = document.querySelector("body > main > div.text-center.mb-3 > button.btn.btn-warning")
 
-    let sp1_1 = document.createElement("th")
-    let sp1 = document.createElement("th")
-    sp1_1.setAttribute("class", "border-bottom-0")
-    sp1.setAttribute("class", "text-nowrap")
-    sp1.innerHTML = "<a>Follows You</a>"
+if (unfollowAllButton) {
+    let parentNode = unfollowAllButton.parentNode
+    let newElement = document.createElement("button")
+    newElement.innerHTML = " Unfollow 0 Non-Followers "
+    
+    newElement.setAttribute("class", "btn btn-success btn-unf")
+    newElement.setAttribute("id", "btn-unf")
+    newElement.addEventListener("click", function (ev) {
+        chrome.storage.sync.set({ autoUnfollowCurrentPage: true })
+        window.location.reload()
+    })
+    parentNode.insertBefore(newElement, unfollowAllButton)
 
-    let sp2_1 = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.border-bottom-0.d-none.d-sm-table-cell")
-    let sp2 = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.d-none.d-sm-table-cell.text-nowrap")
+    newElement = document.createElement("div")
 
-    if (sp2_1 && sp2) {
+    newElement.setAttribute("class", "text-center mb-3")
+    parentNode.insertBefore(newElement, unfollowAllButton)
+}
 
-        let parentDiv_1 = sp2_1.parentNode
-        let parentDiv = sp2.parentNode
+let topTable = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.border-bottom-0.d-none.d-sm-table-cell")
 
-        parentDiv_1.insertBefore(sp1_1, sp2_1)
-        parentDiv.insertBefore(sp1, sp2)
-        
+if (topTable) {
+    let newElement1 = document.createElement("th")
+
+    newElement1.setAttribute("class", "border-bottom-0")
+    
+    let parentNode1 = topTable.parentNode
+    parentNode1.insertBefore(newElement1, topTable)
+}
+
+let bottomTable = document.querySelector("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > thead > tr > th.d-none.d-sm-table-cell.text-nowrap")
+
+if (bottomTable) {
+    
+    let newElement2 = document.createElement("th")
+    newElement2.innerHTML = "<a>Follows Back</a>"
+
+    newElement2.setAttribute("class", "text-nowrap")
+
+    let parentNode2 = bottomTable.parentNode
+
+    parentNode2.insertBefore(newElement2, bottomTable)
+}
+
+let users = {}
+let tables = document.querySelectorAll("tr > td")
 
 
-        let list = document.querySelectorAll("body > main > div > div.card.mb-3 > div.table-responsive > table.table.table-sm.table-hover > tbody > tr")
+tables.forEach((value, key, parent) => {
+    let subElement = value.getElementsByTagName("a").item(0)
+    
+    if (!subElement || subElement.getAttribute("translate") != "no") return
 
-        for (let user of list) {
-            if (user.innerHTML.search("bold") == -1) {
-                let sp1 = document.createElement("td")
-                sp1.innerHTML = '<td title="Not Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/274c.svg" alt="❌"> <b>No</b></td>'
-                
-                let sp2 = user.getElementsByTagName("td")[3]
-                let parentDiv = sp2.parentNode
-                
-                parentDiv.insertBefore(sp1, sp2)
-
-                let buttons = user.getElementsByClassName("btn btn-block btn-sm btn-success")
-                buttons[0].setAttribute("class", "btn btn-block btn-sm btn-success btn-not-follower")
-                buttons[0].setAttribute("id", "UnfollowButton")
-                
-                if (autoUnfollow) {
-                    document.getElementById("UnfollowButton").click()
-                }
-            } else {
-                let sp1 = document.createElement("td")
-                sp1.innerHTML = '<td title="Following" translate="no" class=""><img class="emoji" draggable="false" src="https://s.namemc.com/img/emoji/twitter/2705.svg" alt="✅"> <b>Yes</b></td>'
-                
-                let sp2 = user.getElementsByTagName("td")[3]
-                let parentDiv = sp2.parentNode
-                
-                parentDiv.insertBefore(sp1, sp2)
-            }
-        }
+    users[subElement.getAttribute("title")] = {
+        "class": subElement.getAttribute("class"),
+        "dateElement": tables.item(key + 1),
+        "parentNode": tables.item(key + 1).parentNode,
+        "followButton": (unfollowAllButton) ? tables.item(key + 4).getElementsByTagName("button").item(0) : null
     }
 })
+
+let nonFollowers = 0
+
+for (const user in users) {
+    let isFollowing = (users[user]["class"].search("font-weight-bold") > -1) ? true : false
+    let src = (isFollowing) ? "https://s.namemc.com/img/emoji/twitter/2705.svg" : "https://s.namemc.com/img/emoji/twitter/274c.svg"
+    let alt = (isFollowing) ? "✅" : "❌"
+    let txt = (isFollowing) ? "Yes" : "No"
+    let newElement = document.createElement("td")
+    newElement.innerHTML = `<img class="emoji" draggable="false" src=${src} alt=${alt}> <b>${txt}</b>`
+
+    newElement.setAttribute("translate", "no")
+    users[user]["parentNode"].insertBefore(newElement, users[user]["dateElement"])
+    
+    if (!isFollowing && unfollowAllButton) {
+        users[user]["followButton"].setAttribute("class", `${users[user]["followButton"].getAttribute("class")} btn-not-follower`)
+        
+        nonFollowers += 1
+        document.getElementById("btn-unf").innerHTML = ` Unfollow ${nonFollowers} Non-Follower${(nonFollowers > 1) ? "s" : ""} `
+    }
+}
+
+if (unfollowAllButton) {
+    chrome.storage.sync.get("autoUnfollowCurrentPage", (data) => {
+        let autoUnfollowCurrentPage = data["autoUnfollowCurrentPage"]
+        
+        if (nonFollowers == 0 && autoUnfollowCurrentPage) {
+            chrome.storage.sync.set({ autoUnfollowCurrentPage: false })
+        }
+
+        chrome.storage.sync.get("autoUnfollow", (data) => {
+            let autoUnfollow = data["autoUnfollow"]
+
+
+            if ((autoUnfollow || autoUnfollowCurrentPage) && nonFollowers > 0) {
+                for (const user in users) {
+                    if (users[user]["class"].search("font-weight-bold") == -1) {
+                        users[user]["followButton"].click()
+                    }
+                }
+            }
+        })
+    })
+}
